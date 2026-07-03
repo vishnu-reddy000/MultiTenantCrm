85234712efdd87db975db4119e2cce7673a89c95
jayanth0227 <->
2026-07-03 15:58:47 +0530

    Deployment issue : Generating war file

--- tenantCrm/src/main/java/com/crm/demo/TenantCrmApplication.java
+++ tenantCrm/src/main/java/com/crm/demo/TenantCrmApplication.java
@@ -2,15 +2,20 @@ package com.crm.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-public class TenantCrmApplication {
+public class TenantCrmApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TenantCrmApplication.class, args);
-	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(TenantCrmApplication.class);
+    }
 
+    public static void main(String[] args) {
+        SpringApplication.run(TenantCrmApplication.class, args);
+    }
 }
-
